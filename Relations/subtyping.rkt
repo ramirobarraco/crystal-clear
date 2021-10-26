dce337e9011451adc8792ce839c5f86b43a6f2e1
ramiro barraco <ramirobarraco@gmail.com>
2021-10-26 13:38:13 -0300

    test for subtype and change supreme? for upper_bound?

--- Relations/subtyping.rkt
+++ Relations/subtyping.rkt
@@ -5,22 +5,24 @@
 (define-relation crystal-lang
   subtype ⊆ t × t
   [(subtype t ())]
-  [(subtype t t)]
-  [(subtype t t)
+  [(subtype t_1 t_2)
    (side-condition (term
-                    (supreme? t t)))
+                    (upper_boound? t_1 t_2)))
    ]
   )
 (define-metafunction crystal-lang
-  supreme? : t t -> boolean
-  [(supreme? t ()) #true]
-  [(supreme? st_1 st_1) #true]
-  [(supreme? st_1 st_2) #false]
-  [(supreme? () (st_1 st_2 ...)) #false]
-  [(supreme? (st_1 ...) (st_2 st_3 ...)) ,(and (term(supreme? (st_1 ...) (st_3 ...))) (term(in st_2 (st_1 ...))))]
+  upper_boound? : t t -> boolean
+  [(upper_boound? t ()) #true]
+  [(upper_boound? st_1 st_1) #true]
+  [(upper_boound? st_1 st_2) #false]
+  [(upper_boound? () (st_1 st_2 ...)) #false]
+  [(upper_boound? () st_1) #false]
+  [(upper_boound? (st_1 ...) (st_2 st_3 ...)) ,(and (term(upper_boound? (st_1 ...) (st_3 ...))) (term(in st_2 (st_1 ...))))]
+  [(upper_boound? (st_1 st_2 ...) st_3) ,(term (in st_3 (st_1 st_2 ...)))]
   )
 (define-metafunction crystal-lang
   in : st t -> boolean
-  [(in st_1 ()) #false]
   [(in st_1 (st_2 ... st_1 st_3 ...)) #true]
   [(in st_1 (st_2 ...)) #false])
+
+(provide subtype)
